b7de8a7b732f574c904b85b3a4a8481571fc48db
tesar-tech <33880579+tesar-tech@users.noreply.github.com>
2020-10-13 14:52:45 +0200

    cvt03

---
+++ bizod_cvt03_konvoluce_filtrace_rozmazani_autofocus.m
@@ -0,0 +1,11 @@
+%pracujte s sedotonovou reprezentaci obrazu (je to jednodussi)
+%pracujte s obrazkem kytka256.jpg
+%% vytvorte a vyfiltrujte signal
+%% Obraz M a jadro J 
+%% Poskozeni obrazku vzexponovanym pixelem
+%% vyfiltrovani pixelu vhodnou maskou
+%% Rozmazte obrazek mimo kytku
+%% Ktery ze 2 obrazku je ostrejsi
+%doporucuji to delat pomoci hran a funkce edge.
+%% Hledani nejostrejsiho snimku ve videu.
+
