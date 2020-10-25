0bddb8f6643fa5c4903e056f0b12aa7043e8ebbc
tesar-tech <33880579+tesar-tech@users.noreply.github.com>
2020-10-25 20:56:29 +0100

    cvt05 zadani

---
+++ bizod_cvt05_segmentace_aktivni_kontury_medicinske_obrazy_dicom.m
@@ -0,0 +1,37 @@
+addpath('imgs')
+%% [0.2] nacist obrazek v dicomu ctslice
+%Jaky je to rez?
+%Co je na snimku zobrazeno?
+%Z jakeho je to pristroje?
+
+%prevedte pomoci mat2gray
+
+
+%% [0.3] slice2.dcm
+%Ceho je to snimek? Jaky je to rez?
+
+%% [0.3] zjistit informace o slice2 - kdy byl snimek porizen
+% snimek byl porizen YYYY-MM-DD
+
+%% [2] segmentujte levou plici
+
+%na snimku ctslice
+%pro inicialni segmentaci vyuzijte fci roipoly
+%pro upravu segmentace vyuzijte napr. bwfill
+%upravte parametry fce activecontour, tak aby seg. byla co nejlepsi
+
+%% [0.3] --- MRI hlavy - Nacist a zobrazit
+%soubor mri
+%kouknete co vse obsahuje
+%zobrazte pomoci fce montage (vcetne mapy)
+%predelejte na 3D (misto 4D. Bude se s tim lepe pracovat)
+
+%% [0.3] transverzalni rez (useknuta hlava)
+
+%% [0.3] sagitalni rez (skrze usi "koukam")
+
+%% [0.3] frontalni (koronarni)(vrstvy obliceje)
+
+
+
+
