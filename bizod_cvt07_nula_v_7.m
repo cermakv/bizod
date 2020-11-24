628410e3083fc8774a2cbfbb152f78647705cd91
tesar-tech <33880579+tesar-tech@users.noreply.github.com>
2020-11-24 22:11:02 +0100

    reseni cvt07

--- bizod_cvt07_nula_v_7.m
+++ bizod_cvt07_nula_v_7.m
@@ -1,3 +1,27 @@
 %Muze se hodit
 addpath(genpath('imgs/7v0'));%prida vcetne podslozek
-imageDatastore 
+imds = imageDatastore('imgs/7v0','IncludeSubfolders',true,'LabelSource','foldernames');
+vsechny_obr = imds.readall();
+%% Algoritmus 1 - pocty pixelu v horni a dolni polovine
+%okrouhne bily okraj
+%spocita pixely v horni a dolni polovine
+%jejich pomer je cca stejny u nuly
+% u sedmicky je vyssi
+
+kopieVsechny = vsechny_obr;
+for ii = 1:length(vsechny_obr)
+    aktualni = vsechny_obr{ii};
+    p0b = ~imbinarize(rgb2gray(aktualni));
+    stats = regionprops(p0b,'FilledImage');
+    p0b = stats.FilledImage;
+    pulka = round(size(p0b,1)/2);
+    horniPulka = sum(p0b(1:pulka,:),'all');
+    dolniPulka = sum(p0b(pulka+1:end,:),'all');
+    pomer = horniPulka/dolniPulka;
+    kopieVsechny{ii} = insertText(aktualni,[1 1],num2str(pomer),'FontSize',36);
+    
+end
+montage(kopieVsechny);title('Pomer pixelu v horni/dolni polovine')
+%% Dalsi napady (souckmi):
+%Pocet protnuti transverzalnim rezu
+%kulatost
